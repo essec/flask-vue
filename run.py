8d42a6ef21fda5f083e9071b6c0592fd15e87a84
essec <jakee.indapanya@gmail.com>
2018-12-23 01:03:16 +0700

    adding api endpoint

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from random import *
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__,
             static_folder = './dist/static',
             template_folder = './dist')
+cors = CORS(app, resource={r"/api/*": {"origins": "*"}})
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber' : randint(1, 100)
+    }
+    return jsonify(response)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return render_template("index.html")
+    # if app.debug:
+        # print('in if')
+    return requests.get('http://localhost:8080/{}'.format(path)).text
+    # print('out')
+    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
